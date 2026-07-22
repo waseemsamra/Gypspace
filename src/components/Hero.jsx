34eebcb8cc6109b2296a6f8f458747a7dada779f
waseemsamra <waseemsamra@gmail.com>
2026-07-22 22:47:08 +0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="relative h-screen md:h-[85vh] flex items-center overflow-hidden">
       <div
-        className="absolute inset-0 z-0 bg-cover bg-no-repeat md:bg-center bg-[60%_center] brightness-[0.95]"
+        className="absolute inset-0 z-0 bg-cover bg-no-repeat md:bg-center bg-right brightness-[0.95]"
         style={{ backgroundImage: `url('/hero-image.png')` }}
       />
     </section>
