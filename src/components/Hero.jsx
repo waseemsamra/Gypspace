c50fa1ef56a2975693a250ab98646ddee448e7d0
waseemsamra <waseemsamra@gmail.com>
2026-07-22 21:55:12 +0400

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className="relative h-[70vh] md:h-[85vh] flex items-center overflow-hidden">
+    <section className="relative h-screen md:h-[85vh] flex items-center overflow-hidden">
       <div
-        className="absolute inset-0 z-0 bg-cover bg-center bg-no-repeat brightness-[0.95]"
+        className="absolute inset-0 z-0 bg-cover bg-[center_top] md:bg-center bg-no-repeat brightness-[0.95]"
         style={{ backgroundImage: `url('/hero-image.png')` }}
       />
     </section>
